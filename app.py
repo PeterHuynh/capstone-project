6e2eb14a25fcd457eb28e6fd6254964e86416b99
Peter Huynh <peter.huynh99@gmail.com>
2023-02-08 10:45:16 -0700

    fix test lint

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# from flask import (
-#     Flask,
-#     request,
-#     jsonify
-# )
+from flask import (
+    Flask,
+    request,
+    jsonify
+)
 from flask.logging import create_logger
 import logging
 
