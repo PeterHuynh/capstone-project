04043af75a892e88bfe20d5ab7b0666e16c46db1
Peter Huynh <peter.huynh99@gmail.com>
2023-02-15 09:28:28 -0700

    fix unit test of app code v2.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,5 +2,5 @@ import app
 
 def test_home():
     result = app.home()
-    html = "<h3>Hello, My name is Peter Huynh</h3>"
+    html = "<h3>Hello, My name is Peter Huynh - App Version 2</h3>"
     assert html == result
