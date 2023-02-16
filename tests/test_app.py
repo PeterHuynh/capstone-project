c65f7b5e01c3551b682cd1f987a5fe19f3de719b
Peter Huynh <peter.huynh99@gmail.com>
2023-02-16 12:51:52 -0700

    updated app

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,5 +2,5 @@ import app
 
 def test_home():
     result = app.home()
-    html = "<h3>Hello, My name is Peter Huynh - App Version 2</h3>"
+    html = "<h3>Hello,Test App Version 3</h3>"
     assert html == result
